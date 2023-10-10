edf5bbd57f45f28dfda90f9345a44de5aa5391a8
unknown <190860131036@laxmi.edu.in>
2023-10-10 18:11:21 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const Home = ()=>{
+    return (
+        <div>
+            <section className="flex- start flex-col padding mb-16">
+            <h1>Categories</h1>
+            <h1>posts</h1>
+            <h1>load more</h1>
+            </section>
+        </div>
+    )
+}
+
+export default Home;
